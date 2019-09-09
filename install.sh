3caffab771ac3acf76d67ccebcd60e4652cf9ed5
andrewozhegov <andrew.ozhegov@yandex.ru>
2019-09-10 02:01:24 +0300

    installer upd

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-powerline_installer="https://raw.githubusercontent.com/andrewozhegov/mi-air-13-ubuntu/master/tools/powerline-install.sh"
+powerline_installer_url="https://raw.githubusercontent.com/andrewozhegov/mi-air-13-ubuntu/master/tools/powerline-install.sh"
+powerline_installer="${PWD}/$(basename ${powerline_installer_url})"
 
 ln -sf ~/.vim/vimrc ~/.vimrc
 
-if wget -q ${powerline_installer} ; then
-    chmod 744 $(basename ${powerline_installer})
-    sudo bash $(basename ${powerline_installer}) --vim
+if wget -q ${powerline_installer_url} ; then
+    chmod 744 ${powerline_installer}
+    sudo bash ${powerline_installer} --vim
+    rm -f ${powerline_installer}
 fi
 
 vim -c 'PluginInstall'
