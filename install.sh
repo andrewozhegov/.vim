9b629efa4eebd540142fa6b41053ddffd744ea9d
andrewozhegov <andrew.ozhegov@yandex.ru>
2019-10-22 11:41:43 +0300

    checking for required packages

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+packages_required=exuberant-ctags
+
+for package in ${packages_required} ; do
+    which ${package} || echo "Package '${package}' is required but not found!"
+done
+
 ln -sf ~/.vim/vimrc ~/.vimrc
 vim -c 'PluginInstall'
