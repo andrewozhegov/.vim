3c231593b846ef3dcd8e05fc53db3f8da8399f18
andrewozhegov <andrew.ozhegov@yandex.ru>
2020-04-03 00:21:35 +0300

    specify 'vim-gui-common' as required package (for system clipboard using)

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-packages_required=exuberant-ctags
+packages_required=exuberant-ctags vim-gui-common
 
 for package in ${packages_required} ; do
     which ${package} || echo "Package '${package}' is required but not found!"
