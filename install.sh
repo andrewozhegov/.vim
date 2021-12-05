1b98f631976a6b9f77de7b289390dcbc8b11dce6
andrewozhegov <andrew.ozhegov@yandex.ru>
2021-12-06 01:17:06 +0300

    install script upd

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-packages_required=exuberant-ctags vim-gui-common
+packages_required="exuberant-ctags vim-gui-common"
 
 for package in ${packages_required} ; do
-    which ${package} || echo "Package '${package}' is required but not found!"
+    dpkg -l | grep ${package} || echo "Package '${package}' is required but not found!"
 done
+
+cd ~/.vim
+ls -A ~/.vim/bundle/Vundle.vim && git submodule update --init
 
 ln -sf ~/.vim/vimrc ~/.vimrc
 vim -c 'PluginInstall'
